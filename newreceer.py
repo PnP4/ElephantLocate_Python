597a47079e27566b98d17e3cacaee93d91b6c2b2
DLPDS <tcdlpds@gmail.com>
2016-09-20 18:47:24 +0530

    Complete scearios added.

--- newreceer.py
+++ newreceer.py
@@ -4,16 +4,17 @@ import numpy as np
 from matplotlib import pyplot as plt
 import socket               # Import socket module
 #ser = serial.Serial('/dev/ttyACM0', 9600)
+from random import randint
 
-plt.ion() # set plot to animated
+X = np.linspace(0,2,1000)
+Y = np.linspace(-5,5,1000)
 
-ydata = [0] * 50
-ax1=plt.axes()
-
-# make plot
-line, = plt.plot(ydata)
-plt.ylim([10,40])
-
+plt.ion()
+graph = plt.plot(X,Y)[0]
+N = 600
+T = 1.0 / 800.0
+x = np.linspace(0.0, N * T, N * 10)
+y = np.sin(randint(0,9) * 2.0 * np.pi * x)
 
 
 s = socket.socket()         # Create a socket object
@@ -25,10 +26,17 @@ s.listen(5)                 # Now wait for client connection.
 
 while True:
    c, addr = s.accept()
-   # start data collection
    while True:
-       data = c.recv(1024*1024)
+       data = c.recv(1024 * 1024*1024)
        print data
+       x = np.linspace(0.0, N * T, N * 10)
+       y = np.sin(randint(0, 9) * 2.0 * np.pi * x)
+       #Y = X ** 2 + np.random.random(X.shape)
+       graph.set_xdata(x)
+       graph.set_ydata(y)
+       plt.draw()
+       plt.pause(0.5)
+       point = point + cun
        '''ymin = float(min(ydata)) - 10
        ymax = float(max(ydata)) + 10
        plt.ylim([ymin, ymax])
