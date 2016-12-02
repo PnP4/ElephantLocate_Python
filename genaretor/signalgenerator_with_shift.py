9262d3175217aeb0bd463868d8975ac459b0b887
knadikari <knadikari@gmail.com>
2016-12-02 22:48:21 +0530

    Add bus compatibility

---
+++ genaretor/signalgenerator_with_shift.py
@@ -0,0 +1,12 @@
+import __init__
+
+while True:
+    try :
+        while True:
+            #print "genarate"
+            __init__.socket.send(__init__.gen())
+            
+    except Exception, e:
+        print e
+
+
